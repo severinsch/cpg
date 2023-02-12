edc9e7fec10f4d9012318020c79d52f5163c2d9b
severinsch <severin.schmidmeier@tum.de>
2023-02-12 15:08:42 +0100

    adapted grammar creation from string to support operation productions and simplified tests accordingly

--- cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/helper/OperationTest.kt
+++ cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/helper/OperationTest.kt
@@ -28,117 +28,22 @@ package de.fraunhofer.aisec.cpg.helper
 import de.fraunhofer.aisec.cpg.helper.approximations.CharSetApproximation
 import de.fraunhofer.aisec.cpg.helper.approximations.RegularApproximation
 import de.fraunhofer.aisec.cpg.helper.automaton.GrammarToNFA
-import de.fraunhofer.aisec.cpg.helper.operations.ReplaceBothKnown
-import de.fraunhofer.aisec.cpg.helper.operations.Reverse
-import de.fraunhofer.aisec.cpg.helper.operations.ToUpperCase
-import de.fraunhofer.aisec.cpg.helper.operations.Trim
 import org.junit.jupiter.api.Test
 
 class OperationTest {
 
     @Test
-    fun trimTest() {
-        // A -> op(F)
-        // F -> WX
-        // F -> XW
-        // F -> XF
-        // X -> x
-        // W -> " "
-
-        val g = Grammar()
-        val A = Nonterminal(0, label = "A")
-        val F = Nonterminal(1, label = "F")
-        val X = Nonterminal(2, label = "X")
-        val W = Nonterminal(3, label = "W")
-
-        A.addProduction(UnaryOpProduction(Trim(), F))
-        F.addProduction(ConcatProduction(W, X))
-        F.addProduction(ConcatProduction(X, W))
-        F.addProduction(ConcatProduction(X, F))
-        X.addProduction(TerminalProduction(Terminal("x")))
-        W.addProduction(TerminalProduction(Terminal(" ")))
-        listOf(A, F, X, W).forEach { g.addNonterminal(it) }
-        g.startNonterminal = A
-
-        CharSetApproximation(g).approximate()
-        RegularApproximation(g, setOf(0)).approximate()
-
-        val nfaNoOps = GrammarToNFA(g).makeFA(false)
-        println("NFA:\n${nfaNoOps.toDotString().replace("\\Q", "").replace("\\E", "")}")
-
-        val nfa = GrammarToNFA(g).makeFA(true)
-        println("NFA:\n${nfa.toDotString().replace("\\Q", "").replace("\\E", "")}")
-
-        val pattern = nfa.toRegex()
-        println("Pattern: ${prettyPrintPattern(pattern)}")
-    }
-
-    @Test
-    fun example2() {
-
-        val g = Grammar()
-        val A = Nonterminal(0, label = "A")
-        val C = Nonterminal(1, label = "C")
-        val D = Nonterminal(2, label = "D")
-        val TB = Nonterminal(3, label = "TB")
-        val TA = Nonterminal(4, label = "TA")
-        val TD = Nonterminal(5, label = "TD")
-        val E = Nonterminal(6, label = "E")
-        val TE = Nonterminal(7, label = "TE")
-        val F = Nonterminal(8, label = "F")
-        val TF = Nonterminal(9, label = "TF")
-        val K = Nonterminal(10, label = "K")
-        A.addProduction(UnitProduction(C))
-        C.addProduction(ConcatProduction(TA, A))
-        C.addProduction(ConcatProduction(TB, D))
-        D.addProduction(ConcatProduction(E, TD))
-        // E.addProduction(ConcatProduction(D, TE))
-        E.addProduction(UnitProduction(K))
-        E.addProduction(UnaryOpProduction(ReplaceBothKnown('f', 'x'), K))
-        K.addProduction(ConcatProduction(TF, F))
-        F.addProduction(UnitProduction(TF))
-        F.addProduction(UnitProduction(K))
-        TF.addProduction(TerminalProduction(Terminal("f")))
-        TE.addProduction(TerminalProduction(Terminal("e")))
-        TB.addProduction(TerminalProduction(Terminal("b")))
-        TA.addProduction(TerminalProduction(Terminal("a")))
-        TD.addProduction(TerminalProduction(Terminal("d")))
-        listOf(A, C, D, TB, TA, TD, TE, E, F, K, TF).forEach { g.addNonterminal(it) }
-        g.startNonterminal = A
-        println("Initial grammar:\n${g.printGrammar()}")
-
-        CharSetApproximation(g).approximate()
-        println("After CharSet Approximation:\n${g.printGrammar()}")
-
-        RegularApproximation(g, setOf(0)).approximate()
-        println("After Regular Approximation:\n${g.printGrammar()}")
-
-        val nfa = GrammarToNFA(g).makeFA()
-        println("NFA:\n${nfa.toDotString().replace("\\Q", "").replace("\\E", "")}")
-
-        val pattern = nfa.toRegex()
-        println("Pattern: ${prettyPrintPattern(pattern)}")
-    }
-
-    @Test
     fun exampleOperationProduction() {
-        // A -> F
-        // A -> op(F)
-        // F -> fF
-        // F -> f
-
-        val g = Grammar()
-        val A = Nonterminal(0, label = "A")
-        val F = Nonterminal(2, label = "F")
-        val TF = Nonterminal(3, label = "TF")
-
-        A.addProduction(UnitProduction(F))
-        A.addProduction(UnaryOpProduction(ReplaceBothKnown('f', 'x'), F))
-        F.addProduction(ConcatProduction(TF, F))
-        F.addProduction(UnitProduction(TF))
-        TF.addProduction(TerminalProduction(Terminal("f")))
-        listOf(A, F, TF).forEach { g.addNonterminal(it) }
-        g.startNonterminal = A
+        val grammarDefinition =
+            """
+            A -> E
+            A -> replace[f,x](F)
+            E -> eE
+            E -> e
+            F -> fF
+            F -> f
+        """.trimIndent()
+        val g = grammarStringToGrammar(grammarDefinition)
         println("Initial grammar:\n${g.printGrammar()}")
 
         val nfa = GrammarToNFA(g).makeFA()
@@ -155,37 +60,19 @@ class OperationTest {
 
     @Test
     fun testTaintPropagation() {
-        // A -> op(C)
-        // C -> op(D)
-        // D -> d
-        // C -> op(E)
-        // E -> K
-        // E -> op(K)
-        // K -> fF
-        // F -> f
-        // F -> K
-
-        val g = Grammar()
-        val A = Nonterminal(0, label = "A")
-        val C = Nonterminal(1, label = "C")
-        val D = Nonterminal(2, label = "D")
-
-        val E = Nonterminal(6, label = "E")
-        val F = Nonterminal(8, label = "F")
-        val TF = Nonterminal(9, label = "TF")
-        val K = Nonterminal(10, label = "K")
-        A.addProduction(UnaryOpProduction(Trim(), C))
-        C.addProduction(UnaryOpProduction(ToUpperCase(), D))
-        D.addProduction(TerminalProduction(Terminal("d")))
-        C.addProduction(UnaryOpProduction(Reverse(), E))
-        E.addProduction(UnitProduction(K))
-        E.addProduction(UnaryOpProduction(ReplaceBothKnown('f', 'x'), K))
-        K.addProduction(ConcatProduction(TF, F))
-        F.addProduction(UnitProduction(TF))
-        F.addProduction(UnitProduction(K))
-        TF.addProduction(TerminalProduction(Terminal("f")))
-        listOf(A, C, D, E, F, K, TF, TF).forEach { g.addNonterminal(it) }
-        g.startNonterminal = A
+        val grammarDefinition =
+            """
+        A -> trim(C)
+        C -> toUpperCase(D)
+        D -> d
+        C -> reverse(E)
+        E -> K
+        E -> replace[f,x](K)
+        K -> fF
+        F -> f
+        F -> K
+        """.trimIndent()
+        val g = grammarStringToGrammar(grammarDefinition)
         println("Initial grammar: ${g.printGrammar()}")
 
         CharSetApproximation(g).approximate()
@@ -203,56 +90,61 @@ class OperationTest {
 
     @Test
     fun testReverse() {
-        // S -> X
-        // X -> xS
-        // X -> A
-        // A -> op(C)
-        // C -> op(D)
-        // D -> d
-        // C -> op(E)
-        // E -> K
-        // E -> op(K)
-        // K -> fF
-        // F -> f
-        // F -> K
+        val grammarDefinition =
+            """
+            S -> X
+            X -> xS
+            X -> A
+            A -> trim(C)
+            C -> toUpperCase(D)
+            D -> d
+            C -> reverse(E)
+            E -> K
+            E -> replace[f,x](K)
+            K -> fF
+            F -> a
+            F -> K
+        """.trimIndent()
 
-        val g = Grammar()
-        val S = Nonterminal(11, label = "S")
-        val X = Nonterminal(12, label = "X")
-        val TX = Nonterminal(13, label = "TX")
-        val A = Nonterminal(0, label = "A")
-        val C = Nonterminal(1, label = "C")
-        val D = Nonterminal(2, label = "D")
+        val g = grammarStringToGrammar(grammarDefinition)
 
-        val E = Nonterminal(6, label = "E")
-        val F = Nonterminal(8, label = "F")
-        val TF = Nonterminal(9, label = "TF")
-        val TA = Nonterminal(14, label = "TA")
-        val K = Nonterminal(10, label = "K")
-        S.addProduction(UnitProduction(X))
-        X.addProduction(ConcatProduction(TX, S))
-        X.addProduction(UnitProduction(A))
-        TX.addProduction(TerminalProduction(Terminal("x")))
-        A.addProduction(UnaryOpProduction(Trim(), C))
-        C.addProduction(UnaryOpProduction(ToUpperCase(), D))
-        D.addProduction(TerminalProduction(Terminal("d")))
-        C.addProduction(UnaryOpProduction(Reverse(), E))
-        E.addProduction(UnitProduction(K))
-        E.addProduction(UnaryOpProduction(ReplaceBothKnown('f', 'x'), K))
-        K.addProduction(ConcatProduction(TF, F))
-        F.addProduction(UnitProduction(TA))
-        F.addProduction(UnitProduction(K))
-        TF.addProduction(TerminalProduction(Terminal("f")))
-        TA.addProduction(TerminalProduction(Terminal("a")))
-        listOf(A, C, D, E, F, K, TF, TA, S, X).forEach { g.addNonterminal(it) }
-        g.startNonterminal = S
-        println("Initial grammar: ${g.printGrammar()}")
+        println("Initial grammar:\n${g.printGrammar()}")
 
         CharSetApproximation(g).approximate()
         println("After CharSet Approximation:\n${g.printGrammar()}")
 
         RegularApproximation(g, setOf(0)).approximate()
         println("After Regular Approximation:\n${g.printGrammar()}")
+
+        val nfa = GrammarToNFA(g).makeFA(applyOperations = true)
+        println("NFA:\n${nfa.toDotString().replace("\\Q", "").replace("\\E", "")}")
+
+        val pattern = nfa.toRegex()
+        println("Pattern: ${prettyPrintPattern(pattern)}")
+    }
+
+    @Test
+    fun testReverse2() {
+
+        val grammarDefinition =
+            """
+            A -> E
+            A -> reverse(F)
+            E -> eE
+            E -> e
+            F -> fF
+            F -> f
+        """.trimIndent()
+
+        val g = grammarStringToGrammar(grammarDefinition)
+
+        println("Initial grammar: ${g.printGrammar()}")
+
+        CharSetApproximation(g).approximate()
+        RegularApproximation(g, setOf(0)).approximate()
+
+        val nfaNoOps = GrammarToNFA(g).makeFA(applyOperations = false)
+        println("NFA (no ops):\n${nfaNoOps.toDotString().replace("\\Q", "").replace("\\E", "")}")
 
         val nfa = GrammarToNFA(g).makeFA(applyOperations = true)
         println("NFA:\n${nfa.toDotString().replace("\\Q", "").replace("\\E", "")}")
--- cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/helper/TestHelper.kt
+++ cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/helper/TestHelper.kt
@@ -25,6 +25,8 @@
  */
 package de.fraunhofer.aisec.cpg.helper
 
+import de.fraunhofer.aisec.cpg.helper.operations.*
+
 fun prettyPrintPattern(pattern: String): String {
     return pattern.replace("\\Q", "").replace("\\E", "")
 }
@@ -33,15 +35,58 @@ fun prettyPrintPattern(pattern: String): String {
  * A simple grammar parser for easy grammar creation in tests. The grammar is defined as a string,
  * where each line represents a production. Nonterminals are single uppercase letters, everything
  * else is considered a terminal. For each terminal "a" a new nonterminal "TA" with a single
- * Terminalproduction is created. Operation productions are not supported. Format of the grammar:
- * {single NT} "->" {single symbol} ("|" {single symbol} {single symbol})* Example: S -> Ab A -> Sa
- * | a
+ * TerminalProduction is created.
+ *
+ * Format of the grammar:
+ * - {single NT} "->" {single symbol} ("|" {single symbol} {single symbol})
+ * - Example:
+ *      - S -> Ab
+ *      - A -> Sa | a
+ *
+ * The following operation productions are supported.
+ * - `A -> replace[a,b](B)`
+ * - `A -> reverse(B)`
+ * - `A -> trim(B)`
+ * - `A -> toUpperCase(B)`
+ * - `A -> toLowerCase(B)`
  */
 fun grammarStringToGrammar(grammarString: String): Grammar {
+    val operationRegex = Regex("(?<op>[^\\[(]+)(\\[(?<opArgs>[^]]+)])?\\((?<arg>[^)]+)\\)")
+
     val grammar = Grammar()
     val lines = grammarString.split("\n")
     var currentId: Long = 0
     val nonterminals = mutableMapOf<String, Nonterminal>()
+
+    fun handleOperationProduction(prod: String, left: Nonterminal) {
+        operationRegex.matchEntire(prod.trim())?.let { result ->
+            val op = result.groups["op"]?.value
+            val arg = result.groups["arg"]?.value
+            val opArgs = result.groups["opArgs"]?.value
+            if ((op == null) || (arg == null)) {
+                throw IllegalArgumentException("Invalid operation production: $prod")
+            }
+            val argNT = nonterminals.computeIfAbsent(arg) { Nonterminal(currentId++, label = arg) }
+            val operation =
+                when (op.lowercase()) {
+                    "reverse" -> Reverse()
+                    "replace" -> {
+                        val args = opArgs?.split(",")?.map { it.trim() }
+                        when (args?.size) {
+                            2 -> ReplaceBothKnown(args[0][0], args[1][0])
+                            else ->
+                                throw IllegalArgumentException("Invalid replace arguments: $prod")
+                        }
+                    }
+                    "trim" -> Trim()
+                    "touppercase" -> ToUpperCase()
+                    "tolowercase" -> ToLowerCase()
+                    else -> throw IllegalArgumentException("Unknown operation: $prod")
+                }
+            left.addProduction(UnaryOpProduction(operation, argNT))
+        }
+    }
+
     lines
         .map { it.trim() }
         .filter { it.isNotEmpty() }
@@ -55,29 +100,40 @@ fun grammarStringToGrammar(grammarString: String): Grammar {
 
             val productions = parts[1].split("|")
             productions.forEach { prod ->
-                val nts =
-                    prod
-                        .toCharArray()
-                        .filter { !it.isWhitespace() }
-                        .map { c ->
-                            if (!c.isUpperCase()) {
-                                // Terminal
-                                val terminalNT =
-                                    nonterminals.computeIfAbsent("T${c.uppercase()}") {
+                println("Parsing Production $prod")
+                if (prod.contains("(") && prod.contains(")")) {
+                    handleOperationProduction(prod, left)
+                } else {
+                    val nts =
+                        prod
+                            .toCharArray()
+                            .filter { !it.isWhitespace() }
+                            .map { c ->
+                                if (!c.isUpperCase()) {
+                                    // If the TC nonterminal already exists, return it
+                                    nonterminals["T${c.uppercase()}"]?.let {
+                                        return@map it
+                                    }
+                                    // Terminal
+                                    val terminalNT =
+                                        nonterminals.computeIfAbsent("T${c.uppercase()}") {
+                                            Nonterminal(currentId++, label = it)
+                                        }
+                                    terminalNT.addProduction(
+                                        TerminalProduction(Terminal(c.toString()))
+                                    )
+                                    return@map terminalNT
+                                } else {
+                                    return@map nonterminals.computeIfAbsent(c.toString()) {
                                         Nonterminal(currentId++, label = it)
                                     }
-                                terminalNT.addProduction(TerminalProduction(Terminal(c.toString())))
-                                return@map terminalNT
-                            } else {
-                                return@map nonterminals.computeIfAbsent(c.toString()) {
-                                    Nonterminal(currentId++, label = it)
                                 }
                             }
-                        }
-                when (nts.size) {
-                    1 -> left.addProduction(UnitProduction(nts.first()))
-                    2 -> left.addProduction(ConcatProduction(nts[0], nts[1]))
-                    else -> throw IllegalArgumentException("Invalid production: $prod")
+                    when (nts.size) {
+                        1 -> left.addProduction(UnitProduction(nts.first()))
+                        2 -> left.addProduction(ConcatProduction(nts[0], nts[1]))
+                        else -> throw IllegalArgumentException("Invalid production: $prod")
+                    }
                 }
             }
         }
