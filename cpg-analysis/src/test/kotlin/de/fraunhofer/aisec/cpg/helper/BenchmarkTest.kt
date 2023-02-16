c6cc4075f1e96cfce095717d00d67270c833c710
severinsch <severin.schmidmeier@tum.de>
2023-02-17 00:04:27 +0100

    improved benchmark test

--- cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/helper/BenchmarkTest.kt
+++ cpg-analysis/src/test/kotlin/de/fraunhofer/aisec/cpg/helper/BenchmarkTest.kt
@@ -27,6 +27,7 @@ package de.fraunhofer.aisec.cpg.helper
 
 import de.fraunhofer.aisec.cpg.TestUtils
 import de.fraunhofer.aisec.cpg.TranslationManager
+import de.fraunhofer.aisec.cpg.analysis.fsm.NFA
 import de.fraunhofer.aisec.cpg.frontends.java.JavaLanguage
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.Expression
 import de.fraunhofer.aisec.cpg.helper.approximations.CharSetApproximation
@@ -36,9 +37,9 @@ import de.fraunhofer.aisec.cpg.passes.EdgeCachePass
 import de.fraunhofer.aisec.cpg.passes.IdentifierPass
 import de.fraunhofer.aisec.cpg.passes.StringPropertyHotspots
 import de.fraunhofer.aisec.cpg.passes.StringPropertyPass
+import java.io.BufferedWriter
 import java.io.File
 import java.io.FileOutputStream
-import java.io.OutputStream
 import java.nio.file.Path
 import kotlin.io.path.deleteExisting
 import kotlin.io.path.listDirectoryEntries
@@ -56,13 +57,22 @@ data class TestInput(
     val lineNumber: Int? = null,
     val hotspotType: StringPropertyHotspots.HotspotType? = null,
     val analyzeAllHotspots: Boolean = false,
+    val createDFA: Boolean = false,
 )
 
 data class TestResult(
     val name: String,
     val error: Boolean,
     val result: TestData?,
-)
+) {
+    fun toCsvRow(): String =
+        "\"${this.name}\",${this.error},${this.result?.grammarCreationDuration?.inWholeMicroseconds},${this.result?.charsetApproxDuration?.inWholeMicroseconds},${this.result?.regularApproxDuration?.inWholeMicroseconds},${this.result?.automatonCreationDuration?.inWholeMicroseconds},${this.result?.dfaCreationDuration?.inWholeMicroseconds},${this.result?.toRegexDuration?.inWholeMicroseconds},${this.result?.totalDuration?.inWholeMicroseconds},${this.result?.grammarSize?.first},${this.result?.grammarSize?.second},${this.result?.approximatedGrammarSize?.first},${this.result?.approximatedGrammarSize?.second},${this.result?.automatonSize?.first},${this.result?.automatonSize?.second},${this.result?.dfaSize?.first},${this.result?.dfaSize?.second},${this.result?.regexSize},\"${this.result?.regex}\"\n"
+
+    companion object {
+        const val csvHeader =
+            "Test name,Error,Grammar creation time [μs],Charset approximation time [μs],Regular approximation time [μs],Automaton creation time [μs],DFA creation time [μs],Regex creation time [μs],Total time [μs],Original grammar nonterminals,Original grammar productions,Approximated grammar nonterminals,Approximated grammar productions,Automaton nodes,Automaton edges,DFA nodes,DFA edges,Regex size,Regex\n"
+    }
+}
 
 data class TestData(
     val grammarCreationDuration: Duration,
@@ -72,6 +82,8 @@ data class TestData(
     val approximatedGrammarSize: Pair<Int, Int>, // (nonterminals, productions)
     val automatonCreationDuration: Duration,
     val automatonSize: Pair<Int, Int>, // (states, edges)
+    val dfaCreationDuration: Duration?, // (states, edges)
+    val dfaSize: Pair<Int, Int>?,
     val toRegexDuration: Duration,
     val regexSize: Int,
     val totalDuration: Duration =
@@ -85,6 +97,12 @@ data class TestData(
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BenchmarkTest {
+
+    /**
+     * Benchmarks all test cases in the CWE89_SQL_Injection part of the Juliet test suite. Writes
+     * the results to a CSV file. Warning: Running all inputs takes a long time, consider splitting
+     * it in batches or just taking the first n (~200) inputs.
+     */
     @Test
     fun juliet() {
         val topPath =
@@ -123,16 +141,63 @@ class BenchmarkTest {
                 }
             return inputs
         }
-
+        // even though we copy the files to a temporary directory, CPG creation still runs into an
+        // endless loop/OOM at some point?
+        // workaround: split these inputs into batches of 200 and manually run them separately
         val inputs = listOf("s01", "s02", "s03", "s04").flatMap { getInputs(it) }
 
         println("Got ${inputs.size} test cases totaling ${inputs.sumOf { it.files.size }} files")
-        val results = performBenchmarks(inputs, printResults = true)
-        println("Got ${results.size} results")
-
-        FileOutputStream("juliet_benchmarks.csv").use { it.writeCsv(results) }
+        val julietFileWriter = FileOutputStream("juliet.csv").bufferedWriter()
+        julietFileWriter.write(TestResult.csvHeader)
+        julietFileWriter.flush()
+        performBenchmarks(inputs) { julietFileWriter.writeCsvRow(it) }
+        julietFileWriter.close()
     }
 
+    /**
+     * Runs benchmarks for the Tricky and StringProperties examples and writes the results to CSV
+     * files. Runs each test 100 times to get better results.
+     */
+    @Test
+    fun simpleBenchmarks() {
+        val path = Path.of("src", "test", "resources", "string_properties_benchmarks")
+        val tricky =
+            TestInput(
+                name = "Tricky",
+                files = listOf("Tricky.java").map { path.resolve(it).toFile() },
+                nodeName = "res",
+                lineNumber = 24,
+            )
+        val stringProperties =
+            TestInput(
+                name = "StringProperties",
+                files = listOf("StringProperties.java").map { path.resolve(it).toFile() },
+                nodeName = "res",
+                lineNumber = 14,
+            )
+        val trickyDfa = tricky.copy(name = "${tricky.name}_dfa", createDFA = true)
+        val spDfa = stringProperties.copy(name = "${stringProperties.name}_dfa", createDFA = true)
+        // manually run separately for better results, just comment the other ones out
+        repeatAndWrite(tricky, 100, "tricky.csv")
+        repeatAndWrite(stringProperties, 100, "stringProperties.csv")
+        repeatAndWrite(trickyDfa, 100, "tricky_dfa.csv")
+        repeatAndWrite(spDfa, 100, "stringProperties_dfa.csv")
+    }
+
+    private fun repeatAndWrite(input: TestInput, times: Int, fileName: String) {
+        val resultFileWriter = FileOutputStream(fileName).bufferedWriter()
+        resultFileWriter.write(TestResult.csvHeader)
+        resultFileWriter.flush()
+        repeat(times) { no ->
+            performBenchmarks(
+                listOf(input.copy(name = "${input.name} #$no")),
+            ) {
+                resultFileWriter.writeCsvRow(it)
+            }
+        }
+    }
+
+    /** Analyzes some fixed hotspots in some given inputs and prints the results. */
     @Test
     fun fixedExamples() {
         val path = Path.of("src", "test", "resources", "string_properties_benchmarks")
@@ -204,21 +269,28 @@ class BenchmarkTest {
                     hotspotType = StringPropertyHotspots.HotspotType.DATABASE,
                 ),
             )
-        performBenchmarks(inputs)
+        performBenchmarks(inputs, resultAcceptor = ::printResult)
     }
 
+    // the acceptor is used because collecting the results in a list takes a lot of memory
+    /**
+     * Runs the benchmarks for the given inputs and passes the results to the given acceptor. The
+     * acceptor is called for each input separately and if no acceptor is given, the results are
+     * discarded.
+     * @param inputs the inputs to run the benchmarks for
+     * @param resultAcceptor the acceptor to call for each result
+     */
     private fun performBenchmarks(
         inputs: List<TestInput>,
-        printResults: Boolean = true
-    ): List<TestResult> {
+        resultAcceptor: ((TestResult) -> Unit) = {}
+    ) {
         val newPath = Path.of("/tmp", "string_properties_benchmarks")
-        val results: MutableList<TestResult> = mutableListOf()
 
         for (input in inputs) {
             StringPropertyHotspots.clear()
             if (input.files.isEmpty()) {
                 println("Skipping ${input.name} because no files were found")
-                results += TestResult(input.name, true, null)
+                resultAcceptor(TestResult(input.name, true, null))
                 continue
             }
             // this is needed because CPG creation scans all files in the directory and the large
@@ -234,7 +306,7 @@ class BenchmarkTest {
             newPath.listDirectoryEntries().forEach { it.deleteExisting() }
 
             if (input.analyzeAllHotspots) {
-                println("Analyzing all hotspots for ${input.name}")
+                // println("Analyzing all hotspots for ${input.name}")
                 val hotspots =
                     when (input.hotspotType) {
                         StringPropertyHotspots.HotspotType.DATABASE ->
@@ -247,12 +319,13 @@ class BenchmarkTest {
                     }
                 for (hotspot in hotspots) {
                     val data = analyzeNode(hotspot)
-                    results +=
+                    resultAcceptor(
                         TestResult(
                             "${input.name}[node ${hotspot.id} at ${hotspot.location}]",
                             false,
                             data
                         )
+                    )
                 }
             } else {
                 val node =
@@ -274,22 +347,19 @@ class BenchmarkTest {
                                 ?: true
                     }
                 if (node == null) {
-                    results += TestResult(input.name, true, null)
                     println("Could not find node for input $input")
+                    resultAcceptor(TestResult(input.name, true, null))
                     continue
                 }
 
-                val data = analyzeNode(node)
-                results += TestResult(input.name, false, data)
+                val data = analyzeNode(node, createDFA = input.createDFA)
+                resultAcceptor(TestResult(input.name, false, data))
             }
         }
-        if (printResults) {
-            printTestResults(results)
-        }
         StringPropertyHotspots.clear()
-        return results
     }
 
+    /** Builds a CPG for the given files with path as the toplevel directory */
     private fun buildCPG(files: List<File>, path: Path) {
         TranslationManager.builder().build().analyze()
         TestUtils.analyzeAndGetFirstTU(files, path, true) {
@@ -300,9 +370,15 @@ class BenchmarkTest {
         }
     }
 
+    /**
+     * Performs all steps from node to regex, measures the duration of each step and collects it
+     * with the sizes of the results
+     * @param node the node to analyze
+     * @param createDFA whether to create a DFA from the NFA before converting it to a regex
+     */
     @OptIn(ExperimentalTime::class)
-    fun analyzeNode(node: Expression): TestData {
-        println("Analyzing node ${node.name} at ${node.location}")
+    fun analyzeNode(node: Expression, createDFA: Boolean = false): TestData {
+        // println("Analyzing node ${node.name} at ${node.location}")
         val (grammar, grammarCreationDuration) = measureTimedValue { createGrammar(node) }
 
         val originalGrammarSize =
@@ -316,8 +392,20 @@ class BenchmarkTest {
             grammar.getAllNonterminals().size to
                 grammar.getAllNonterminals().flatMap { it.productions }.size
 
-        val (automaton, automatonCreationDuration) =
+        var (automaton, automatonCreationDuration) =
             measureTimedValue { GrammarToNFA(grammar).makeFA() }
+
+        val automatonSize =
+            automaton.states.size to automaton.states.flatMap { it.outgoingEdges }.size
+
+        var dfaCreationDuration: Duration? = null
+        var dfaSize: Pair<Int, Int>? = null
+        if (createDFA) {
+            val (dfa, dur) = measureTimedValue { automaton.toDfa() }
+            dfaCreationDuration = dur
+            automaton = NFA(dfa.states)
+            dfaSize = dfa.states.size to dfa.states.flatMap { it.outgoingEdges }.size
+        }
 
         val (pattern, toRegexDuration) = measureTimedValue { automaton.toRegex() }
         return TestData(
@@ -327,64 +415,53 @@ class BenchmarkTest {
             regularApproxDuration = regularApproxDuration,
             approximatedGrammarSize = regularGrammarSize,
             automatonCreationDuration = automatonCreationDuration,
-            automatonSize =
-                automaton.states.size to automaton.states.flatMap { it.outgoingEdges }.size,
+            automatonSize = automatonSize,
+            dfaCreationDuration = dfaCreationDuration,
+            dfaSize = dfaSize,
             toRegexDuration = toRegexDuration,
             regexSize = pattern.length,
             regex = pattern,
         )
     }
 
-    private fun printTestResults(results: List<TestResult>) {
-        val ansiReset = "\u001B[0m"
-        val ansiRed = "\u001B[31m"
-        val ansiGreen = "\u001B[32m"
+    private val ansiReset = "\u001B[0m"
+    private val ansiRed = "\u001B[31m"
+    private val ansiGreen = "\u001B[32m"
 
-        val formatDuration = { duration: Duration, total: Duration ->
-            "$duration (${String.format("%.2f", duration / total * 100)}% of total)"
-        }
+    private fun formatDuration(duration: Duration?, total: Duration) =
+        duration?.let { "$duration (${String.format("%.2f", duration / total * 100)}% of total)" }
+            ?: "N/A"
 
-        println("Test results:")
-        for (result in results) {
-            if (result.error) {
-                println("+++++++++++++++++++++++++++++")
-                println("${ansiRed}Error in test ${result.name}$ansiReset")
-                continue
-            }
-            val data = result.result!!
-            println(
-                """
-                +++++++++++++++++++++++++++++
-                ${ansiGreen}Test "${result.name}"$ansiReset:
-                Grammar creation time:     ${formatDuration(data.grammarCreationDuration, data.totalDuration)}
-                Grammar size:              ${data.grammarSize.first} nonterminals, ${data.grammarSize.second} productions
-                Charset approximation:     ${formatDuration(data.charsetApproxDuration, data.totalDuration)}
-                Regular approximation:     ${formatDuration(data.regularApproxDuration, data.totalDuration)}
-                Approximated grammar size: ${data.approximatedGrammarSize.first} nonterminals, ${data.approximatedGrammarSize.second} productions
-                Automaton creation:        ${formatDuration(data.automatonCreationDuration, data.totalDuration)}
-                Automaton size:            ${data.automatonSize.first} states, ${data.automatonSize.second} edges
-                Regex creation:            ${formatDuration(data.toRegexDuration, data.totalDuration)}
-                Regex size:                ${data.regexSize}
-                Total time:                ${data.totalDuration}
-                Regex:                     ${prettyPrintPattern(data.regex)}
-                """.trimIndent()
-            )
+    private fun printResult(result: TestResult) {
+        if (result.error || result.result == null) {
+            println("+++++++++++++++++++++++++++++")
+            println("${ansiRed}Error in test ${result.name}$ansiReset")
+            return
         }
-        println("+++++++++++++++++++++++++++++")
+        val data = result.result
+        println(
+            """
+            +++++++++++++++++++++++++++++
+            ${ansiGreen}Test "${result.name}"$ansiReset:
+            Grammar creation time:     ${formatDuration(data.grammarCreationDuration, data.totalDuration)}
+            Grammar size:              ${data.grammarSize.first} nonterminals, ${data.grammarSize.second} productions
+            Charset approximation:     ${formatDuration(data.charsetApproxDuration, data.totalDuration)}
+            Regular approximation:     ${formatDuration(data.regularApproxDuration, data.totalDuration)}
+            Approximated grammar size: ${data.approximatedGrammarSize.first} nonterminals, ${data.approximatedGrammarSize.second} productions
+            Automaton creation:        ${formatDuration(data.automatonCreationDuration, data.totalDuration)}
+            Automaton size:            ${data.automatonSize.first} states, ${data.automatonSize.second} edges
+            DFA creation:              ${formatDuration(data.dfaCreationDuration, data.totalDuration)}
+            DFA size:                  ${data.dfaSize?.first ?: "N/A"} states, ${data.dfaSize?.second ?: "N/A"} edges
+            Regex creation:            ${formatDuration(data.toRegexDuration, data.totalDuration)}
+            Regex size:                ${data.regexSize}
+            Total time:                ${data.totalDuration}
+            Regex:                     ${prettyPrintPattern(data.regex)}
+            """.trimIndent()
+        )
     }
 
-    private fun OutputStream.writeCsv(results: List<TestResult>) {
-        val header =
-            """"Test name", "Error", "Grammar creation time [μs]", "Charset approximation time [μs]", "Regular approximation time [μs]", "Automaton creation time [μs]", "Regex creation time [μs]", "Total time [μs]", "Original grammar nonterminals", "Original grammar productions", "Approximated grammar nonterminals", "Approximated grammar productions", "Automaton nodes", "Automaton edges", "Regex size", "Regex""""
-        val writer = this.bufferedWriter()
-        writer.write(header)
-        writer.newLine()
-        results.forEach {
-            writer.write(
-                "\"${it.name}\", ${it.error}, ${it.result?.grammarCreationDuration?.inWholeMicroseconds}, ${it.result?.charsetApproxDuration?.inWholeMicroseconds}, ${it.result?.regularApproxDuration?.inWholeMicroseconds}, ${it.result?.automatonCreationDuration?.inWholeMicroseconds}, ${it.result?.toRegexDuration?.inWholeMicroseconds}, ${it.result?.totalDuration?.inWholeMicroseconds}, ${it.result?.grammarSize?.first}, ${it.result?.grammarSize?.second}, ${it.result?.approximatedGrammarSize?.first}, ${it.result?.approximatedGrammarSize?.second}, ${it.result?.automatonSize?.first}, ${it.result?.automatonSize?.second}, ${it.result?.regexSize}, ${it.result?.regex}"
-            )
-            writer.newLine()
-        }
-        writer.flush()
+    private fun BufferedWriter.writeCsvRow(result: TestResult) {
+        this.write(result.toCsvRow())
+        this.flush()
     }
 }
